1e932ed1db7ce0952b5cd34caa9243d4fd320430
raphaelmadr <82519125+raphaelmadr@users.noreply.github.com>
2026-09-02 11:48:01 -0300

    fix(auth): configure edge middleware and fix nextauth init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXTAUTH_SECRET: "fallback_secret_for_vercel_that_satisfies_nextauth_78910",
+  },
   allowedDevOrigins: ["192.168.102.36", "localhost"],
 
   typescript: {
